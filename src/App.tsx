a22bfb2aef74eb78fb3058900d068469c6f9f0c4
Alex-Zozulia-1997 <oleksandr97zozulia@gmail.com>
2024-06-23 21:49:02 +0200

    f

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="bg-yellow-900">hello</div>
+      <input className="bg-red-200 w-full" type="text" />
+      <div className="bg-slate-800">Black</div>
+    </>
+  );
+}
+
+export default App;
